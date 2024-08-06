90d6b1a8cb5154d548744eeb7958fda393755a20
drago G <dragomir1520@gmail.com>
2024-08-07 02:47:25 +0300

    Back-end и Front-end приемат само един файл. TLDR: Браузъра приема файл с <input type='file'/> и го запаметява с onChange функция към setFormData(). Изпраща с fetch() към сървъра formData към body. Взима response с готова снимка (binary data и image/webp). Прави го на Blob, защото за това се ползва. Прави Blob-a на URL. <a> с href и download attribute. TODO: server handles multiple files, .zip, изпраща правилни имена на файловете. Front-end изкарва всички файлове с линк и мета данни към тях. Download. Front-end правилно display на качени файлове и точни корекции, че в момента може само bulk и без алт.

--- back-end/server.js
+++ back-end/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.post('/convert', upload.single('file'), async (req, res) => {
+app.post('/convert', upload.single('files'), async (req, res) => {
   try {
     const { compression, width, height } = req.body;
     const fileBuffer = req.file.buffer;
@@ -23,9 +23,10 @@ app.post('/convert', upload.single('file'), async (req, res) => {
 
     const imageProcessor = new ImageProcessor(fileBuffer, options);
     const convertedImage = await imageProcessor.convertToWebP();
-    console.log(convertedImage);
+    console.log(`This is the converted image ${convertedImage}`);
     res.set('Content-Type', 'image/webp');
     res.send(convertedImage);
+    Buffer.fill(0);
   } catch (error) {
     console.error('Error converting file', error);
     res.status(500).send('Error converting file');
--- front-end/src/components/Layout.jsx
+++ front-end/src/components/Layout.jsx
@@ -1,15 +1,56 @@
 import ImageGrid from './ImageGrid';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+
 
 
 export default function Layout() {
 
   const [formData, setFormData] = useState({});
+  const [files, setFiles] = useState({});
+  const [filesCount, setFilesCount] = useState(0);
+  const [compression, setCompression] = useState(80);
+  const [url, setUrl] = useState("")
   
-  function submitForm(event){
+  
+  async function submitForm(event){
     event.preventDefault();
+    const formData = new FormData();
+    files.forEach((file, index)=>{ 
+      formData.append('files', file)
+    })
+    formData.append('width', 1200)
+    formData.append('height', 1200)
+    formData.append('compression',compression)
+    try {
+      let response = await fetch("http://localhost:5000/convert",{
+        method: 'POST',
+        body: formData,
+      })
+      let imageBlob = await response.blob()
+      const urlTmp = window.URL.createObjectURL(imageBlob)
+      console.log(imageBlob)
+      setUrl(urlTmp);
+    } catch (e) {
+      console.log(e);
+      
+    }
+      
+    
     
   }
+  function handleFileChange(e) {
+    let files = e.target.files;
+    setFilesCount(files.length)
+    setFiles(Array.from(files));
+    
+  }
+  
+  
+  function handleCompressionChange(e) {
+    console.log(e.target.value);
+    setCompression(e.target.value);
+   }
 
   return (
     <>  
@@ -20,15 +61,16 @@ export default function Layout() {
         <ImageGrid />
 
         <section>
-          <input type='file' className='rounded-xl mr-2 text-lg bg-sky-400	' multiple/>
+          <input type='file' onChange={handleFileChange} className='rounded-xl mr-2 text-lg bg-sky-400	' multiple/>
           <button type='button' className='rounded-xl px-4 border-2 border-zinc-100	p-1 text-lg mr-2 bg-yellow-500	' onClick={submitForm}>Convert</button>
           <button type='button' className='rounded-xl px-4 border-2 border-zinc-100	p-1 text-lg bg-lime-400	'>Download All!</button>
+          <a href={url} download>Download here tmp</a>
         </section>
         
         <section>
           <h3>Bulk edit</h3>
           <h4 className='inline-block mr-1'>Compression:</h4>
-          <input type='text' name='bulkCompression' value="80"/>
+          <input type='text' name='bulkCompression' value={compression} onChange={handleCompressionChange}/>
           <h4 className='inline-block ml-2.5 mr-1'>Width:</h4>
           <input type='text' name='bulkWidth'/>
           <h4 className='inline-block ml-2.5 mr-1'>Height:</h4>
